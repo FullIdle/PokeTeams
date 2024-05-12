4d4533cfe02ee114bfd36e64ecde7e35bbd1832e
FullIdle <2675423979@qq.com>
2024-05-12 17:15:03 +0800

    允许仓库名字带颜色 仓库列表用GUI代替

--- src/main/java/me/gsqfi/poketeams/poketeams/commands/ListCmd.java
+++ src/main/java/me/gsqfi/poketeams/poketeams/commands/ListCmd.java
@@ -1,16 +1,11 @@
 package me.gsqfi.poketeams.poketeams.commands;
 
-import com.google.common.collect.Lists;
-import me.gsqfi.poketeams.poketeams.PlayerData;
-import me.gsqfi.poketeams.poketeams.helper.ComponentHelper;
+import me.gsqfi.poketeams.poketeams.gui.TeamsListGui;
 import me.gsqfi.poketeams.poketeams.helper.StringHelper;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,7 +21,7 @@ public class ListCmd extends AbstractTabExecutor{
             return false;
         }
         Player player = (Player) sender;
-        ComponentHelper helper = new ComponentHelper("§3§lList\n");
+        /*ComponentHelper helper = new ComponentHelper("§3§lList\n");
         ArrayList<String> list = Lists.newArrayList(PlayerData.getConfig().getConfigurationSection(player.getName()).getKeys(false));
         int lastIndex = list.size() - 1;
         for (int i = 0; i < list.size(); i++) {
@@ -37,7 +32,8 @@ public class ListCmd extends AbstractTabExecutor{
             }
         }
         BaseComponent[] components = helper.create();
-        player.spigot().sendMessage(components);
+        player.spigot().sendMessage(components);*/
+        player.openInventory(new TeamsListGui(player,0).getInventory());
         return false;
     }
 
